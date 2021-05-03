bfb5257fde75f54ee6ef1941e0175d47b4049b37
ClaySamuelC <claysamuelc@gmail.com>
2021-05-03 15:03:47 -0700

    Now prunes the locked nodes from the list before shuffling, and puts them back where they belong afterwards

--- colorgame/src/models/createShuffledMatrix.js
+++ colorgame/src/models/createShuffledMatrix.js
@@ -1,32 +1,70 @@
-const swap = (aX, aY, bX, bY, matrix) => {
-  let temp = matrix[aY][aX];
-  matrix[aY][aX] = matrix[bY][bX];
-  matrix[bY][bX] = temp;
+const pruneMatrix = (matrix, nodesToRemove) => {
+  const prunedNodes = [];
+
+  for (let i = nodesToRemove.length - 1; i >= 0; i--) {
+    let node = matrix[nodesToRemove[i].y].splice(nodesToRemove[i].x, 1);
+    prunedNodes.unshift(...node);
+  }
+
+  return prunedNodes;
 };
 
-const shuffleMatrix = (matrix) => {
-  for (let i = matrix.length - 1; i >= 0; i--) {
-    for (let j = matrix[0].length - 1; j >= 0; j--) {
-      let randomIndex = Math.floor(Math.random() * (i * matrix.length + j));
-      let randomI = Math.floor(randomIndex / matrix.length);
-      let randomJ = randomIndex % matrix[0].length;
+const flattenMatrix = (matrix) => {
+  return [].concat(...matrix);
+};
 
-      swap(randomJ, randomI, j, i, matrix);
-    }
+const shuffleArr = (arr) => {
+  for (let i = arr.length - 1; i >= 0; i--) {
+    let rndI = Math.floor(Math.random() * i);
+
+    let tmp = arr[rndI];
+    arr[rndI] = arr[i];
+    arr[i] = tmp;
+  }
+}
+
+const insertCoords = (arr, nodesToInsert, width) => {
+  for (let i = 0; i < nodesToInsert.length; i++) {
+    arr.splice(nodesToInsert[i].y * width + nodesToInsert[i].x, 0, nodesToInsert[i]);
   }
 };
 
-const createShuffledMatrix = (matrix) => {
+const arrToMatrix = (arr, width) => {
+  const matrix = [];
+
+  let i, k;
+  for (i = 0, k = -1; i < arr.length; i++) {
+    if (i % width === 0) {
+      k++;
+      matrix[k] = [];
+    }
+    matrix[k].push(arr[i]);
+  }
+
+  return matrix;
+}
+
+const shuffleMatrix = (matrix, lockedCoords) => {
+  let width = matrix[0].length;
+  let lockedNodes = pruneMatrix(matrix, lockedCoords);
+  const arr = flattenMatrix(matrix);
+  shuffleArr(arr);
+  insertCoords(arr, lockedNodes, width);
+
+  return arrToMatrix(arr, width);
+};
+
+const createShuffledMatrix = (matrix, lockedCoords) => {
   // construct a copy of matrix by values
   const newMatrix = matrix.map((arr) => {
     return arr.slice();
   });
 
   // shuffle the array
-  shuffleMatrix(newMatrix);
+  const shuffledMatrix = shuffleMatrix(newMatrix, lockedCoords);
 
   // return shuffled matrix
-  return newMatrix;
+  return shuffledMatrix;
 };
 
 export default createShuffledMatrix;
