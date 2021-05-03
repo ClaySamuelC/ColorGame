c4af8930d6234813b5d400f4d1ae7aad96ddd076
ClaySamuelC <claysamuelc@gmail.com>
2021-05-02 17:52:55 -0700

    change format of field in DOM

--- colorgame/src/views/App.js
+++ colorgame/src/views/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import ColorNode from'./ColorNode.js';
 import field from '../controllers/getField.js';
+import specs from '../models/specs.js';
 
 function App() {
 
   return (
     <div className="App">
       <p>Color Game!</p>
-      <div className="field">
+      <div className="field" style={{gridTemplate: 'repeat(' + specs.height + ', 1fr) / repeat(' + specs.width + ', 1fr)'}}>
         {field.map((row, i) => {
           return row.map((color, j) => {
             return <ColorNode color={color} key={'' + i + j} className='node'/>
