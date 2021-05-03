d7b8940cbecdd5dca56a5c512d21bc74985dae98
ClaySamuelC <claysamuelc@gmail.com>
2021-05-02 17:44:17 -0700

    Change getField to return a shuffled version of the field

--- colorgame/src/controllers/getField.js
+++ colorgame/src/controllers/getField.js
@@ -1,6 +1,8 @@
-import generateField from '../models/colorAt.js';
+import generateField from '../models/generateField.js';
 import specs from '../models/specs.js';
+import createShuffledMatrix from '../models/createShuffledMatrix';
 
 const field = generateField(specs.width, specs.height, specs.topLeft, specs.topRight, specs.botLeft, specs.botRight);
+const shuffledField = createShuffledMatrix(field);
 
-export default field;
+export default shuffledField;
