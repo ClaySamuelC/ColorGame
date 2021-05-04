67339d3077c01571fe16adcb186607b882c47c5b
ClaySamuelC <claysamuelc@gmail.com>
2021-05-03 17:15:49 -0700

    Add stateful class-based component with click handling

--- colorgame/src/views/ColorNode.js
+++ colorgame/src/views/ColorNode.js
@@ -1,15 +1,36 @@
-function ColorNode({node}) {
-  return (
-    <div
-    id={'' + node.y + node.x}
-    className='colorNode'
-    style={{backgroundColor: ('rgb(' + node.color.red + ', ' + node.color.green + ', ' + node.color.blue + ')')}}
-    >
-      <span className='locked'>
-        {node.locked ? 'x' : ''}
-      </span>
-    </div>
-  )
+import React from 'react';
+
+class ColorNode extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isClicked: false};
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    if (!this.props.node.locked) {
+      this.setState(state => ({
+        isClicked: !state.isClicked
+      }));
+    }
+  }
+
+  render() {
+    return (
+      <div
+        id={'' + this.props.node.y + this.props.node.x}
+        style={{backgroundColor: ('rgb(' + this.props.node.color.red + ', ' + this.props.node.color.green + ', ' + this.props.node.color.blue + ')')}}
+        className = {'colorNode' + (this.state.isClicked ? ' clicked' : '')}
+        onClick={this.handleClick}
+      >
+        <span className='locked'>
+          {this.props.node.locked ? 'x' : ''}
+        </span>
+      </div>
+    );
+  }
 }
 
 export default ColorNode;
